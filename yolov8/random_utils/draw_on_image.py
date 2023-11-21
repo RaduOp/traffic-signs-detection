c86711128ba19f07249513194656272156ba8c67
unknown <georgianradu062@gmail.com>
2023-11-21 07:01:15 +0200

    Implemented and documented util functions that will most likely not change

--- yolov8/random_utils/draw_on_image.py
+++ yolov8/random_utils/draw_on_image.py
@@ -1,19 +1,28 @@
+"""
+This file contains a collection of methods meant for displaying rectangles or text on an image.
+It uses the default cv2 methods (rectangle and putText) but adds a little bit of customization.
+They are created for visualization purposes and are great for debugging.
+"""
+
 from typing import Union
 
 import cv2
 import numpy as np
-from label_transposing import yolo_to_corner_values
+from label_transposing import check_coordinates_type
 
 
-def check_coordinates_type(coords: list[Union[int, float]]):
-    if coords[0] < 1 and coords[1] < 1 and coords[2] < 1 and coords[3] < 1:
-        return yolo_to_corner_values(coords)
-    return coords
+def draw_custom_rectangle(frame: np.ndarray, coords: list[Union[int, float]],
+                          color: tuple[int, int, int] = (0, 255, 0), thickness=1) -> np.ndarray:
+    """
+    Draw a rectangle on an image at given coordinates. It uses the cv2.rectangle method but does
+    checks for coordinates and is customized beforehand. Might add additional steps in the future.
 
-
-def draw_custom_rectangle(frame: np.ndarray, coords: list[Union[int, float]], color: tuple[int,
-int, int] = (0, 255, 0),
-                          thickness=1):
+    :param frame: the image you want to draw on
+    :param coords: coordinates in yolo or corner format
+    :param color: in case you want to change the preset color
+    :param thickness: border thickness
+    :return: image with drawn rectangle
+    """
     x1, y1, x2, y2 = check_coordinates_type(coords)
 
     cv2.rectangle(frame, (x1, y1), (x2, y2),
@@ -22,7 +31,20 @@ int, int] = (0, 255, 0),
 
 
 def draw_rectangle_size(frame: np.ndarray, coords: list[int], color: tuple[int] = (255, 0, 0),
-                        thickness: int = 1, font_scale: float = 0.5):
+                        thickness: int = 1, font_scale: float = 0.5) -> np.ndarray:
+    """
+    Draw the sizes of the rectangle (in pixels). It uses the cv2.putText method to draw line
+    sizes in pixels, does checks for coordinates and is customized (color, font, font_scale,
+    thickness).
+
+    :param frame: image to draw on
+    :param coords: coordinates in yolo or corner format
+    :param color: in case you want to change the preset color
+    :param thickness: text thickness
+    :param font_scale: font size
+    :return: image with drawn rectangle sizes (you might want to draw the rectangle first)
+    """
+
     x1, y1, x2, y2 = check_coordinates_type(coords)
     frame = cv2.putText(frame, str(y2 - y1), (x2 - 50, y1 + ((y2 - y1) // 2)),
                         cv2.FONT_HERSHEY_SIMPLEX,
@@ -39,7 +61,20 @@ def draw_rectangle_size(frame: np.ndarray, coords: list[int], color: tuple[int] 
 
 def draw_rectangle_name(frame: np.ndarray, coords: list[int], class_name: str, conf: float,
                         color: tuple[int] = (255, 0, 0), thickness: int = 1,
-                        font_scale: float = 0.5):
+                        font_scale: float = 0.5) -> np.ndarray:
+    """
+    Write a name next to a rectangle. Very useful for class names, does checks for coordinates,
+    and is customized (color, font size, and thickness). Used mostly for detection boxes names.
+    :param frame: image to draw on
+    :param coords: coordinates in yolo or corner format
+    :param class_name: the name you want to write
+    :param conf: confidence for detection
+    :param color: in case you want to change the preset color
+    :param thickness: text thickness
+    :param font_scale: text size
+    :return: image with name drawn for the given rectangle (you might want to draw the rectangle
+    first)
+    """
     x1, y1, x2, y2 = check_coordinates_type(coords)
     cv2.putText(frame, f"{class_name}, {round(conf, 2)}", (x1, y1 - 20),
                 cv2.FONT_HERSHEY_SIMPLEX,
--- yolov8/random_utils/label_transposing.py
+++ yolov8/random_utils/label_transposing.py
@@ -1,8 +1,34 @@
+"""
+A collection of methods that help with coordinates transposing. Sometimes you need yolo format,
+and sometimes you don't, these methods make it easier.
+"""
+
 from typing import Union
 
 
+def check_coordinates_type(coords: list[Union[int, float]]) -> list[int]:
+    """
+    This function checks the coordinates for their type (yolo/corner format) and always returns
+    them in the corner format so drawing on image can be easier
+
+    :param coords: coordinates from label file, yolo or corner format
+    :return: [x1,y1,x2,y2]
+    """
+    if coords[0] < 1 and coords[1] < 1 and coords[2] < 1 and coords[3] < 1:
+        return yolo_to_corner_values(coords)
+    return coords
+
+
 def yolo_to_corner_values(coordinates: list[Union[int, float]], image_height: int = 640,
-                          image_width: int = 640):
+                          image_width: int = 640) -> list[int]:
+    """
+    A method that transforms yolo format coordinates into min corner max corner coordinates.
+
+    :param coordinates: a list of coordinates in yolo format
+    :param image_height: 640 by default but can be changed
+    :param image_width: 640 by default but can be changed
+    :return: coordinates in min corner max corner format (coords are cast to int for ease of use)
+    """
     center_x, center_y, width, height = coordinates
 
     x_min = int((center_x - width / 2) * image_width)
@@ -10,20 +36,24 @@ def yolo_to_corner_values(coordinates: list[Union[int, float]], image_height: in
     x_max = int((center_x + width / 2) * image_width)
     y_max = int((center_y + height / 2) * image_height)
 
-    return x_min, y_min, x_max, y_max
+    return [x_min, y_min, x_max, y_max]
 
 
 def corner_values_to_yolo(coordinates: list[Union[int, float]], image_height: int = 640,
-                          image_width: int = 640):
+                          image_width: int = 640) -> list[float]:
+    """
+    A method that transforms min corner max corner coordinates to yolo format.
+
+    :param coordinates: a list of min corner max corner type coordinates
+    :param image_height: 640 by default but can be changed
+    :param image_width: 640 by default but can be changed
+    :return: coordinates in yolo format according to image size
+    """
     x_min, y_min, x_max, y_max = coordinates
-    x_center = (x_min + x_max) / 2.0
-    y_center = (y_min + y_max) / 2.0
-    width = x_max - x_min
-    height = y_max - y_min
 
-    x_center /= image_width
-    y_center /= image_height
-    width /= image_width
-    height /= image_height
+    x_center = ((x_min + x_max) / 2.0) / image_width
+    y_center = ((y_min + y_max) / 2.0) / image_height
+    width = (x_max - x_min) / image_width
+    height = (y_max - y_min) / image_height
 
-    return x_center, y_center, width, height
+    return [x_center, y_center, width, height]
