792a478a37b282a40716e14c94a32b0c6474af70
unknown <georgianradu062@gmail.com>
2024-02-08 13:24:27 +0200

    Refactoring and lintint

--- traffic_signs_recognition/YoloV8_experiments/detectors.py
+++ traffic_signs_recognition/YoloV8_experiments/detectors.py
@@ -3,16 +3,27 @@ from ultralytics import YOLO
 
 
 class YoloDetector:
-    def __init__(self, path_to_model: str, image_size: int):
+    def __init__(self, path_to_model: str, image_size: int = None, verbose=True):
         self.image_size = image_size
         self.path_to_model = path_to_model
         self.model = self._init_model_from_path()
+        self.verbose = verbose
+
+    def change_image_size(self, new_img_size):
+        self.image_size = new_img_size
 
     def _init_model_from_path(self):
         return YOLO(self.path_to_model)
 
-    def _use_model_to_detect(self, frame: np.ndarray):
-        return self.model.predict(frame, imgsz=self.image_size)
+    def _use_model_to_detect(self, frame: np.ndarray, image_size: int = None):
+        if image_size is not None:
+            return self.model.predict(frame, imgsz=image_size, verbose=self.verbose)
+        if self.image_size is not None:
+            return self.model.predict(
+                frame, imgsz=self.image_size, verbose=self.verbose
+            )
+
+        return self.model.predict(frame, imgsz=frame.shape[1], verbose=self.verbose)
 
     def get_class_names(self):
         return self.model.names
--- traffic_signs_recognition/YoloV8_experiments/use_model_live.py
+++ traffic_signs_recognition/YoloV8_experiments/use_model_live.py
@@ -4,7 +4,7 @@ This file is meant for live testing of the trained model.
 import cv2
 import numpy as np
 from mss import mss
-import argparse
+
 from traffic_signs_recognition import YoloDetector
 from traffic_signs_recognition.random_utils import (
     draw_custom_rectangle,
@@ -12,7 +12,57 @@ from traffic_signs_recognition.random_utils import (
 )
 
 
+def test_on_video(
+    image_detector: YoloDetector, video_path: str, output_path: str = "output.avi"
+):
+    """Test the model on a video. Saves the output."""
+    cap = cv2.VideoCapture(video_path)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    fps = cap.get(5)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+    class_names = image_detector.get_class_names()
+    i = 0
+    s = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Perform object detection on the frame
+        boxes = image_detector.get_results_as_list(frame)
+        if len(boxes) > 0:
+            for box in boxes:
+                if box["conf"] > 0.5:
+                    frame = draw_custom_rectangle(frame, box["coords"])
+                    frame = draw_rectangle_name(
+                        frame,
+                        box["coords"],
+                        class_name=class_names[box["class_index"]],
+                        conf=box["conf"],
+                    )
+
+        # Write the frame with detections to the output video
+        out.write(frame)
+
+        i += 1
+        if i % 1800 == 0:
+            s += 30
+            print(f"Seconds done: {s}")
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    # Release the video capture and writer objects
+    cap.release()
+    out.release()
+    # Close all OpenCV windows
+    cv2.destroyAllWindows()
+
+
 def test_live_on_screen_with_multiple_monitors(image_detector: YoloDetector) -> None:
+    """Test model live."""
     sct = mss(compression_level=1)
     monitor_number = 2
     monitors = mss().monitors
@@ -46,26 +96,15 @@ def test_live_on_screen_with_multiple_monitors(image_detector: YoloDetector) -> 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
-        elif key == ord("n"):  # Press 'n' to switch to the next monitor
+        if key == ord("n"):  # Press 'n' to switch to the next monitor
             monitor_number = (monitor_number + 1) % len(monitors)
 
     cv2.destroyAllWindows()
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Testing the model on video or live on main "
-    )
-
-    user_args = parser.parse_args()
-    return user_args
-
-
 if __name__ == "__main__":
-    user_args = parse_arguments()
-    dataset_yaml_path = "datasets/data.yaml"
-    model_ckpt_path = "../YOLOV8/runs/detect/train8/weights/last.pt"
+    MODEL_CKPT = "../YOLOV8/runs/detect/train8/weights/best.pt"
+    IMAGE_DETECTOR = YoloDetector(MODEL_CKPT, 1920)
 
-    image_detector = YoloDetector(model_ckpt_path, 1920)
-
-    test_live_on_screen_with_multiple_monitors(image_detector)
+    # test_live_on_screen_with_multiple_monitors(image_detector)
+    test_live_on_screen_with_multiple_monitors(IMAGE_DETECTOR)
